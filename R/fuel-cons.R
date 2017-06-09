2e90e0fbdcbd759af357d248a8a0000567607f6e
DanB <dannyboybrennan@gmail.com>
2017-06-09 12:41:52 +0100

    corrected fuel costs function

--- R/fuel-cons.R
+++ R/fuel-cons.R
@@ -3,33 +3,43 @@
 #' This function generates a table of fuel consumption costs per km for the 
 #' specified vector of traffic speeds (in kph) for the standard vehicle and
 #' fuel types.
-#' @param speed A vector of traffic speeds. Defaults to between 1 and 150 kph
+#' @param speed A vector of traffic speeds.
 #' @param fuel_cons_param Fuel consumption parameters available in package 
 #' @param fuel_split 2011 Fleet vehicle splits by fuel type available in 
 #' package
 #' @param fuel_costs Petrol and diesel fuel costs in cents from 2011 available
 #'  in package
 #' @keywords cba, fuel, consumption
-#' @return A table of scheme costs per year to be input into the cbaTable() function
+#' @return A table of scheme costs per year to be input into the cbaTable() 
+#' function
 #' @export
 
-fuel_costs_km <- function() {
+fuel_costs_km <- function(speed, 
+                          fuel_cons_param, 
+                          fuel_split, 
+                          fuel_cost_2011) {
     
-    fuel_cons_table <- function(speed = c(1:150), 
-                                fuel_cons_param = fuel_cons_param, 
-                                fuel_split = fuel_split, 
-                                fuel_costs = fuel_cost_2011) {
+    fuel_cons_table <- function(speed, 
+                                fuel_cons_param, 
+                                fuel_split, 
+                                fuel_cost_2011) {
+        
         
         fuel_cons <- function(data) {
-            v <- c(1:150)
+            v <- speed
             return (data_frame(speed = v,
-                               cons = (data$a + (data$b * v) + (data$c * (v ^2)) +
+                               cons = (data$a + (data$b * v) + 
+                                           (data$c * (v ^2)) +
                                            (data$d * (v ^ 3))) / v))
         }
         
-        param <- fuel_cons_param %>% 
+        cons <- fuel_cons_param %>% 
             group_by(vehicle) %>% 
-            nest()
+            nest() %>% 
+            separate(vehicle, into = c("fuel", "veh"), sep = "_")  %>% 
+            mutate(cons = map(data, fuel_cons)) %>% 
+            select(-data) %>% 
+            unnest()
         
         split <- fuel_split %>% 
             gather(fuel, prop, 2:3) %>% 
@@ -37,17 +47,10 @@ fuel_costs_km <- function() {
             mutate(vehicle = paste(fuel, veh, sep = "_")) %>% 
             select(vehicle, prop)
         
-        cons <- param %>% 
-            separate(vehicle, into = c("fuel", "veh"), sep = "_") %>% 
-            mutate(cons = map(data, fuel_cons)) %>% 
-            select(-data) %>% 
-            unnest()
-        
-        cost <- fuel_costs
+        cost <- fuel_cost_2011
         
         cons_veh <- cons %>% 
             mutate(vehicle = paste(fuel, veh, sep = "_")) %>%
-            filter(veh == v) %>% 
             left_join(split, by = "vehicle") %>%
             left_join(cost, by = "fuel") %>%
             mutate(cost_per_km = (cons * price) / 100) %>% 
@@ -58,13 +61,13 @@ fuel_costs_km <- function() {
         return (cons_veh)
     }
 
-    costs_table <- data_frame(vehicle = speed) %>% 
-        mutate(cons = map(vehicle, fuel_cons_table, 
+    costs_table <- data_frame(v = speed) %>% 
+        mutate(cons = map(v, fuel_cons_table, 
                           fuel_cons_param = fuel_cons_param,
                           fuel_split = fuel_split,
-                          fuel_costs = fuel_costs)) %>% 
+                          fuel_cost_2011 = fuel_cost_2011)) %>% 
         unnest() %>% 
-        select(-vehicle)
+        select(-v)
     
     return(costs_table)
 }
